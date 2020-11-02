4399ed6406ebd1bd444bbe19d19a735c9309d8a2
RAN Github <r_a_n@yahoo.com>
2020-11-02 14:37:57 -0600

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes)
+        {
+            ViewData["Message"] = string.Format("Hello, {0}", name);
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
+
+//using Microsoft.AspNetCore.Mvc;
+//using System.Text.Encodings.Web;
+
+//namespace MvcMovie.Controllers
+//{
+//    public class HelloWorldController : Controller
+//    {
+//        // 
+//        // GET: /HelloWorld/
+
+//        public string Index()
+//        {
+//            return "This is my default action...";
+//        }
+
+//        // 
+//        // GET: /HelloWorld/Welcome/ 
+
+//        public string Welcome(string name, int numTimes = 1)
+//        {
+//            //return "This is the Welcome action method...";
+//            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+//        }
+
+//        public string Abcde(int num)
+//        {
+//            //return "abcde";
+//            return HtmlEncoder.Default.Encode($"Abcde - {num}");
+//        }
+//    }
+//}
